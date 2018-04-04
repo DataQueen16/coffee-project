b36c1db7194b4fcc5526ae9e1fb6953ecd4e6b8f
John Cook <cooks.johns@gmail.com>
2018-04-04 16:28:05 -0500

    dropdown/ search

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 "use strict";
 //  ========================  TEST  =================
-function changeRoast() {
-    var x = document.getElementById("mySelect");
-    document.getElementById("demo").innerHTML = x.options[x.selectedIndex].text;
-}
+// function changeRoast() {
+//     var x = document.getElementById("mySelect");
+//     document.getElementById("demo").innerHTML = x.options[x.selectedIndex].text;
+// }
+
+
 
 function displayCoffees(arr) {
     var coffeesContainer ='<div id="coffee-labels-container">';
@@ -11,13 +13,13 @@ function displayCoffees(arr) {
         coffeesContainer += '<h2 class="coffee-name">' + el.name + '</h2>';
     });
     coffeesContainer += '</div>';
-    document.getElementById('coffee-container').innerHTML = coffeesContainer;
+    document.getElementById('coffees').innerHTML = coffeesContainer;
 }
 
-document.getElementById('').addEventListener('keyup', function() {
+document.getElementById('myInput').addEventListener('keyup', function() {
     var newCoffees = [];
     coffees.forEach(function (el) {
-        if(el.name.toLowerCase().match(document.getElementById('search').value)) {
+        if(el.name.toLowerCase().match(document.getElementById('myInput').value)) {
             newCoffees.push(el);
         }
     });
@@ -93,4 +95,5 @@ var roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+roastSelection.addEventListener('change', updateCoffees);
+// submitButton.addEventListener('keyUp', displayCoffees);
